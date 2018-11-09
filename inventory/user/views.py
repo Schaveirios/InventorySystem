d00e21132a031baa7fa9072040fcdc4bc26eb4cb
agent909 <agentvis2@gmail.com>
2018-11-09 23:23:19 +0800

    changed user_logout return function

--- inventory/user/views.py
+++ inventory/user/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from user.forms import UserForm, UserProfileInfoForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     return render(request,'login/login.html')
@@ -17,7 +18,7 @@ def special(request):
 @login_required
 def user_logout(request):
     logout(request)
-    return HttpResponseRedirect(reverse('index'))
+    return redirect("/")
 
 
 def register(request):
