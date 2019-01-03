d65dd1341dd43023332243b627e52bf09d4c1091
agent909 <agentvis2@gmail.com>
2019-01-03 11:30:48 +0800

    template update

--- inventory/report/forms.py
+++ inventory/report/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class WeeklyReportForm(forms.Form):
-    year = forms.IntegerField()
-    weekNumber = forms.IntegerField(min_value=1)
+    year = forms.ChoiceField(widget=forms.Select(attrs={"class":"form-control","id":"yearSelect"}))
+    weekNumber = forms.IntegerField(min_value=1, widget=forms.NumberInput(attrs={"class":"form-control","id":"weekSelect"}))
 
     def __str__(self):
         return "Weekly Report"
